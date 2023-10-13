41ca844733428a37be67c7529203ee70b9a784ef
Roman <roman.troitskiy@icloud.com>
2023-10-13 13:30:15 +0700

    final_v0

--- vvpd2.py
+++ vvpd2.py
@@ -1,7 +1,7 @@
 '''vvpd3'''
 def menu():
     '''program menu'''
-    print('1 --> first task \n2 --> second task \n3 --> third task \n stop --> finish program')
+    print('1 --> first task \n2 --> second task \n3 --> third task \nstop --> finish program')
 
 def func1(a:list, b:list):
     '''first task'''
@@ -26,3 +26,37 @@ def func3(a:list, b:list):
         if a.count(l) == b.count(l) == 1:
             res_3.append(l)
     return res_3
+
+F = ''
+while F != "stop":
+    menu()
+    F = str(input())
+    if F == 'stop':
+        print('program finished')
+        break
+    try:
+        a = list(map(int, input().split(',')))
+    except ValueError:
+        print('not all values in list "a" are integers'
+              'enter "a" again')
+        try:
+            a = list(map(int, input().split(',')))
+        except ValueError:
+            print('program break')
+            break
+    try:
+        b = list(map(int, input().split(',')))
+    except ValueError:
+        print('not all values in list "b" are integers'
+              'enter "b" again')
+        try:
+            b = list(map(int, input().split(',')))
+        except ValueError:
+            print('program break')
+            break
+    if F == '1':
+        print(func1(a, b))
+    elif F == '2':
+        print(func2(a, b))
+    elif F == '3':
+        print(func3(a, b))
