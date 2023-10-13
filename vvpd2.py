23ede4c1ab73615504f898bea90df14847b69ee3
Roman <roman.troitskiy@icloud.com>
2023-10-13 12:59:59 +0700

    second

--- vvpd2.py
+++ vvpd2.py
@@ -1,27 +1,52 @@
 '''vvpd3'''
-from random import randint
-
-a = [randint(1, 100) for i in range(100)]
-b = [randint(1, 100) for j in range(100)]
+def menu():
+    '''program menu'''
+    print('1 --> first task \n2 --> second task \n3 --> third task \n stop --> finish program')
 
 def func1(a:list, b:list):
+    '''first task'''
     res_1 = []
     for i in a:
         if a.count(i) == 1 and b.count(i) > 1:
             res_1.append(i)
     return res_1
+
 def func2(a:list, b:list):
+    '''second task'''
     res_2 = []
     for j in a:
         if b.count(j) == 0:
             res_2.append(j)
     return res_2
+
 def func3(a:list, b:list):
+    '''fird task'''
     res_3 = []
     for l in a:
         if a.count(l) == b.count(l) == 1:
             res_3.append(l)
     return res_3
-print(func1(a,b))
-print(func2(a,b))
-print(func3(a,b))
+
+F = ''
+while F != "stop":
+    menu()
+    F = str(input())
+    if F == 'stop':
+        print('program finished')
+        break
+    try:
+        a = list(map(int, input().split(',')))
+    except ValueError:
+        print('not all values in list "a" are integers')
+        break
+    try:
+        b = list(map(int, input().split(',')))
+    except ValueError:
+        print('not all values in list "b" are integers')
+        break
+    if F == '1':
+        print(func1(a, b))
+    elif F == '2':
+        print(func2(a, b))
+    elif F == '3':
+        print(func3(a, b))
