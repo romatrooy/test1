2c1037f58c809475bb8361263717eea00ac0590c
Roman <roman.troitskiy@icloud.com>
2023-10-13 13:06:13 +0700

    first task

--- vvpd2.py
+++ vvpd2.py
@@ -2,4 +2,12 @@
 def menu():
     '''program menu'''
     print('1 --> first task \n2 --> second task \n3 --> third task \n stop --> finish program')
-git
+
+def func1(a:list, b:list):
+    '''first task'''
+    res_1 = []
+    for i in a:
+        if a.count(i) == 1 and b.count(i) > 1:
+            res_1.append(i)
+    return res_1
+
